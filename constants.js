5eed9ecb8723e7ead85253f2d34888348ba2d2a5
Anton Kulakov <anton.kulakoff@ya.ru>
2024-07-22 23:05:29 +0300

    refactor constants

--- constants.js
+++ constants.js
@@ -6,6 +6,7 @@ export const SERVICE_PREFIX = process.env.SERVICE_PREFIX || 'sys_intercom';
 export const AUTH_ENABLED = process.env.AUTH_ENABLED === 'false';
 export const AUTH_USER = process.env.AUTH_USER;
 export const AUTH_PASS = process.env.AUTH_PASS;
+
 // Intercom models
 export const BEWARD_DKS = 'BEWARD DKS'
 export const BEWARD_DS = 'BEWARD DS'
