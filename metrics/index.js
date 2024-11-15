cf3f6360ed76ca37c5f1297501b841ba34d49a0b
Anton Kulakov <anton.kulakoff@ya.ru>
2024-11-15 15:30:49 +0300

    refactor: update stricture

--- metrics/index.js
+++
@@ -1,4 +0,0 @@
-// device specific metrics
-export { getBewardMetrics } from './devices/beward.js'
-export { getQtechMetrics } from './devices/qtech.js'
-export { getAkuvoxMetrics } from './devices/akuvox.js'
