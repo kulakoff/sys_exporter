8eb4cd9279b781bbd9054c11d3046a2aeb5f0cbe
Anton Kulakov <anton.kulakoff@ya.ru>
2024-11-15 15:25:52 +0300

    feat: update project structure

---
+++ app/routes/routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { AUTH_ENABLED } from "../constants.js";
+import basicAuthMiddleware from "../middleware/auth.js";
+import metricsRoute from "./metrics.js"
+import probeRoute from "./probe.js"
+
+const router = express.Router();
+
+// auth
+if (AUTH_ENABLED === true) {
+    console.log(`${new Date().toLocaleString("RU")} | Auth enabled`);
+    router.use(basicAuthMiddleware);
+}
+
+router.use('/metrics', metricsRoute); //global metrics route
+router.use('/probe', probeRoute); // Probe request route
+
+export default router;
